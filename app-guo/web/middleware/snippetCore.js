c171f2025064735022f02ab009b360035e3545de
lymos <lymos@lym.com>
2023-05-11 17:42:16 +0800

    u

--- app-guo/web/middleware/snippetCore.js
+++ app-guo/web/middleware/snippetCore.js
@@ -80,6 +80,7 @@ export function SnippetCore() {
             title
             description
             onlineStoreUrl
+            onlineStorePreviewUrl
             featuredImage {
               url
             }

43b47868d0eb444e1c6be0b8384bbefee286509d
a.com <a.com>
2023-07-27 13:31:59 +0800

    u

--- ads-bar-builder/export-env.sh
+++ ads-bar-builder/export-env.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-export SHOPIFY_API_KEY=cea5899eeb25a81856ce4f691daed195 SHOPIFY_API_SECRET=7f2c2dc33d4b8fe61dc200ec829dca83 HOST=https://app.xtoool.com SCOPES=write_products,write_customers,write_draft_orders,read_themes PORT=3000 NODE_ENV=production
+SHOPIFY_API_KEY=cea5899eeb25a81856ce4f691daed195 
+SHOPIFY_API_SECRET=7f2c2dc33d4b8fe61dc200ec829dca83 
+HOST=https://app.xtoool.com 
+SCOPES=write_products,write_customers,write_draft_orders,read_themes 
+PORT=3000 
+NODE_ENV=production
+export SHOPIFY_API_KEY
+export SHOPIFY_API_SECRET
+export HOST
+export SCOPES
+export PORT
+export NODE_ENV
